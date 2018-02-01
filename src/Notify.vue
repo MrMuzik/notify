ba6cb2eb89d43c24d55d7020bee7afa690df60d0
Bryan <bmarkham@bizjournals.com>
2018-02-01 15:07:30 -0500

    Adding some styling.

--- src/Notify.vue
+++ src/Notify.vue
@@ -4,8 +4,11 @@
   width: 150px;
   top: 20px;
   display: inline-block;
-  padding: 15px;
-  background-color: #aaa;
+  padding: 15px 15px 25px 15px;
+  background-color: #fafafa;
+  box-shadow: 0 0 3px rgba(30, 30, 30, 0.25);
+  font-size: 10px;
+  font-family: sans-serif;
   color: #222;
   right: 20px;
   // Keep this in sync with timeout
@@ -21,29 +24,50 @@
     right: -190px;
   }
 
+  .name {
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+
+  .timeout {
+    font-size: 8px;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    line-height: 1;
+  }
+
+  p {
+    * + & {
+      margin-top: 1em;
+    }
+    line-height: 1;
+  }
+
   &:hover {
     cursor: pointer;
-    ::after {
+    &::after {
       content: "Close";
       position: absolute;
       top: 10px;
       right: 10px;
       font-family: sans-serif;
       line-height: 1;
-      font-size: 10px;
+      font-size: 8px;
+      opacity: 0.6;
       display: inline-block;
     }
   }
-}
-.display-none {
-  display: none !important;
 }
 </style>
 
 <template>
 <transition name="slide-fade">
   <div class="notify" v-if="active" :style="topPos" @click="updateActive(false)">
+      <div class="name">Notification name</div>
       <p>{{message}}</p>
+      <div class="timeout">Disappears in 3 seconds</div>
   </div>
 </transition>
 </template>
